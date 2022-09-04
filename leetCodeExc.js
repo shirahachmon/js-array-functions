b76d35f578647def9e6899b195293cf3fb00b2fe
shirahachmon <75565282+shirahachmon@users.noreply.github.com>
2022-09-04 10:09:59 +0300

    minor

--- leetCodeExc.js
+++ leetCodeExc.js
@@ -271,3 +271,109 @@ var maxArea = function(height) {
 
 
 console.log(maxArea([1,4,5,8,3,4,9,7,3]))
+
+
+/**------------------------------------------------------------------------------------------------------------- */
+/**------------------------------------------------------------------------------------------------------------- */
+
+
+var romanToInt = function (s) {
+    let num = 0;
+    let obj = {
+      M: 1000,
+      D: 500,
+      C: 100,
+      L: 50,
+      X: 10,
+      V: 5,
+      I: 1,
+    };
+    for (let i = 0; i < s.length; i++) {
+      if (obj[s[i]] < obj[s[i + 1]]) {
+        num = num + obj[s[i + 1]] - obj[s[i]];
+        i++;
+      } else {
+        num = num + obj[s[i]];
+      }
+    }
+    return num;
+  };
+
+  console.log(romanToInt("DXLII")) 
+
+  
+
+/**------------------------------------------------------------------------------------------------------------- */
+/**------------------------------------------------------------------------------------------------------------- */
+
+
+var intToRoman = function(num) {
+    //create the list..
+    const M = ["", "M", "MM", "MMM"];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    //return solution with this procedure...
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
+};
+
+console.log( intToRoman("1223"))
+
+
+
+/**------------------------------------------------------------------------------------------------------------- */
+/**------------------------------------------------------------------------------------------------------------- */
+
+const longestCommonPrefix = (strs = ['flo', 'flow', 'flight']) => {
+    strs.sort();
+    const minLength = Math.min(strs[0].length, strs[strs.length - 1].length);
+    let i = 0;
+    let prefix = '';
+    while (i < minLength && strs[0][i] == strs[strs.length - 1][i]) {
+      i++;
+      prefix = strs[0].substring(0, i);
+    }
+    return prefix;
+  };
+
+  console.log(longestCommonPrefix())
+
+  
+/**------------------------------------------------------------------------------------------------------------- */
+/**------------------------------------------------------------------------------------------------------------- */
+var threeSum = function(nums) {
+	var rtn = [];
+	if (nums.length < 3) {
+		return rtn;
+	}
+	nums = nums.sort(function(a, b) {
+		return a - b;
+	});
+	for (var i = 0; i < nums.length - 2; i++) {
+		if (nums[i] > 0) {
+			return rtn;
+		}
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+		for (var j = i + 1, k = nums.length - 1; j < k;) {
+			if (nums[i] + nums[j] + nums[k] === 0) {
+				rtn.push([nums[i], nums[j], nums[k]]);
+				j++;
+				k--;
+				while (j < k && nums[j] == nums[j - 1]) {
+					j++;
+				}
+				while (j < k && nums[k] == nums[k + 1]) {
+					k--;
+				}
+			} else if (nums[i] + nums[j] + nums[k] > 0) {
+				k--;
+			} else {
+				j++;
+			}
+		}
+	}
+	return rtn;
+};
+console.log(threeSum([-1, 1,0,3,-1,4,-4,0,1,-3]))
